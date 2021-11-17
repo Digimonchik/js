d73074b659d26a4328fc7b2b5431d8ec5acd3f1a
Digimonchik <da.logvinenko@gmail.com>
2021-11-17 19:24:03 +0300

    update

---
+++ jest_tests/task3/getMinSquaredNumber.js
@@ -0,0 +1,6 @@
+const getMinSquaredNumber = arr => {
+    const newArr = arr.map(num => Math.abs(num))
+    console.log(newArr)
+    const minSquaredNumber = Math.min(...newArr) * Math.min(...newArr);
+    return minSquaredNumber
+}
---
+++ jest_tests/task3/getMinSquaredNumber.test.js
@@ -0,0 +1,6 @@
+import { getMinSquaredNumber } from "./getMinSquaredNumber";
+
+it('gets squared min number', () => {
+    const result = getMinSquaredNumber[-7, -22, 5];
+    expect(result).toEqual(25)
+})
