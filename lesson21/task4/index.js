b959523e4aaa20053839b83570c7c68ce903949f
Digimonchik <da.logvinenko@gmail.com>
2021-11-27 22:33:17 +0300

    update

--- lesson21/task4/index.js
+++ lesson21/task4/index.js
@@ -1,4 +1,16 @@
-function getTitle() {
+export function getTitle() {
     let elem = document.querySelector('.title');
     return elem.textContent
+}
+export function getDescription() {
+    let elem = document.querySelector('.about');
+    return elem.innerText
+}
+export function getPlans() {
+    let elem = document.querySelector('.plans')
+    return elem.innerHTML
+}
+export function getGoal() {
+    let elem = document.querySelector('.goal')
+    return elem.outerHTML
 }
