8f54f82152b8b3e95e2254e718a5bab1a2da0ce8
Digimonchik <da.logvinenko@gmail.com>
2022-01-14 23:33:30 +0300

    update

--- testtasks/index.js
+++ testtasks/index.js
@@ -1,3 +1,8 @@
-function findNeedle(haystack) {
-  return `found the needle at position ${haystack.indexOf("needle")}`;
+function findUniq(arr) {
+  const x = arr.filter((elm) => elm === arr[0]);
+  const y = arr.filter((elm) => elm !== arr[0]);
+
+  return x.length > y.length ? y[0] : x[0];
 }
+
+findUniq([1, 0, 0]);
