17262028fcc7bf33e44b192012b3e12b27567893
Digimonchik <da.logvinenko@gmail.com>
2022-01-21 15:26:09 +0300

    update

--- lesson36/task2 copy/index.js
+++ lesson36/task2 copy/index.js
@@ -3,7 +3,7 @@ import { onSearchUser } from "./src/onsearchUser.js";
 const userAvatarElem = document.querySelector(".user__avatar");
 const showUserButtonELem = document.querySelector(".name-form__btn");
 
-userAvatarElem.src = "https://avatars3.githubusercontent.com/u10002 ";
+userAvatarElem.src = "https://avatars3.githubusercontent.com/u10001";
 
 showUserButtonELem.addEventListener("click", () => {
   onSearchUser();
